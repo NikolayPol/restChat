9c311282dcc6d906caa59abecc6c0c7bf3e4d912
NikolayPol <N5445@yandex.ru>
2022-01-05 10:00:10 +0300

    initial commit

---
+++ src/main/java/com/example/restchat/RestchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.restchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestchatApplication.class, args);
+    }
+
+}
